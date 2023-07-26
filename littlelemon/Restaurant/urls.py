d5d18935362dd2648b05d8551c2d407fa71f3fc5
Mostafizur Rahman <csemrm@gmail.com>
2023-07-26 12:18:40 -0700

    booking api

--- littlelemon/Restaurant/urls.py
+++ littlelemon/Restaurant/urls.py
@@ -1,7 +1,10 @@
 #define URL route for index() view
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+
 ]
+#urlpatterns += router.urls
--- littlelemon/Restaurant/views.py
+++ littlelemon/Restaurant/views.py
@@ -1,5 +1,50 @@
 from django.shortcuts import render
+from .serializers import UserSerializer, MenuSerializer,BookingSerializer
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Menu, Booking
+
+"""
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+"""
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+"""
+class UserListCreateView(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+class UserRetrieveUpdateDeleteView(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+"""
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
